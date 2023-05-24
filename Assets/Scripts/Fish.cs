c74cc2d44d6d5479e0355232132c92c593a3a8ae
İsa <ulutepe42@gmail.com>
2023-05-24 20:56:09 +0300

    Added little Fish Movement

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+
+    Rigidbody2D _rb;
+    [SerializeField] private float _speed;
+    //Balýgýn aþaðý yukarý rotasyonunun deðiþmesi için.
+    int angle;
+    int maxAngel = 20;
+    int minAngle = -60;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, _speed);
+
+        }
+        //AÇI ÝÇÝN ÝÞLEMLER.
+        if (_rb.velocity.y > 0)
+        {
+            if (angle <= maxAngel)
+                angle = angle + 4;
+        }
+        else if (_rb.velocity.y < -2.5f)
+        {
+            if (angle > minAngle)
+            {
+                angle -= 2;
+            }
+        }
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
